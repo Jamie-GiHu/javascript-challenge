d336566637c24e80d4623ca57bd009186e6dd1fc
Jamie-GiHu <65794716+Jamie-GiHu@users.noreply.github.com>
2020-09-02 18:07:25 +1000

    initial commit level 2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -22,33 +22,21 @@ data.forEach((sightingReport) => {
     });
 });
 
-// Use d3 to select the button and the form
 var button = d3.select("#filter-btn");
 var form = d3.select("#form");
 
-// Create event handlers
 button.on("click",runEnter);
 form.on("submit",runEnter);
 
-// complete the event handler function for the form
 function runEnter() {
-
-    // Prevent the page from refreshing
     d3.event.preventDefault();
-
-    // Select the input element and get the raw HTML node
     var inputElement = d3.select("#datetime");
-
-    // Get the value property of the input element
     var inputValue = inputElement.property("value");
     
     // Clear table values
     tbody.html("");
-
-    // Filter through sighting report according to 'datetime' value from input element
     var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
-    
-    // Loop through 'filteredData' to insert table rows and populate table values
+    console.log(filteredData);
     filteredData.forEach((sightingReport) => {
         var row = tbody.append("tr");
         Object.entries(sightingReport).forEach(([key,value]) => {
